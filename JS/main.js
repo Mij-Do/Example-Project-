50331d4742afc9809e1c5f0016de1865c46b3b45
ahmed <ahmedmijdo2@gmail.com>
2025-02-14 17:39:08 +0200

    add team section & back to top button

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,32 @@ drop.addEventListener("click", function () {
         item.style.visibility = "hidden";
     }
 });
+
+drop.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+        event.preventDefault();
+        if (item.style.visibility === "hidden") {
+            item.style.visibility = "visible";
+        } else {
+            item.style.visibility = "hidden";
+        }
+    }
+});
+
+let backToTop = document.getElementById("back-to-top");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+};
+
+backToTop.addEventListener ('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
